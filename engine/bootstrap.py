aa33ea02678b0ee1df0689b5216059d0d6de2eba
aslamlogic <furrkh.aslam@proton.me>
2026-03-31 13:18:41 +0100

    Update bootstrap.py

--- engine/bootstrap.py
+++ engine/bootstrap.py
@@ -1,1 +1,21 @@
+name: Bootstrap MBH
 
+on:
+  push:
+    paths:
+      - 'specs/**'
+
+jobs:
+  bootstrap:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v4
+
+      - name: Install deps
+        run: pip install requests
+
+      - name: Run bootstrap
+        env:
+          CLAUDE_API_KEY: ${{ secrets.CLAUDE_API_KEY }}
+        run: python engine/bootstrap.py
